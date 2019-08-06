9b2381f3288f00166000a4774a58e32a974195a8
Mayank Baranwal <baranwalm2002@gmail.com>
2019-08-06 14:04:30 +0100

    Merged Codes

--- esa-xt85/CseHardware/esa-xt85/Week1C16.asm
+++ esa-xt85/CseHardware/esa-xt85/Week1C16.asm
@@ -6,11 +6,11 @@ org 9000h
 dcr a
 jz ADD
 dcr a
-; jz SUB
-; dcr a
-; jz MUL
-; dcr a
-; jz DIV
+jz SUB
+dcr a
+jz MUL
+dcr a
+jz DIV
 
 ADD:
 	lxi h, 8840H
@@ -35,6 +35,63 @@ ADD:
 	sta 8901h
 	mov a,b
 	sta 8902h
+	jmp OVER
+
+SUB:
+	LHLD 8840H
+	XCHG
+	LHLD 8842H
+	MOV A, E
+	SUB L
+	STA 8900H
+	MOV A, D
+	SBB H
+	STA 8901H
+	jmp OVER
+
+MUL:
+	LHLD 8840h
+	SPHL
+	LHLD 8842h
+	XCHG
+	LXI H, 0000H
+	LXI B, 0000H
+	LBL2:
+	DAD SP
+	JNC LBL
+	INX B
+	LBL:
+	DCX D
+	MOV A, E
+	ORA D
+	JNZ LBL2
+	SHLD 8900h
+	MOV L, C
+	MOV H, B
+	SHLD 8902h
+	jmp OVER
+
+DIV:
+	LXI B, 0000H
+	LHLD 8840h
+	XCHG
+	LHLD 8842h
+	LBL2:
+	MOV A, L
+	SUB E
+	MOV L, A
+	MOV A, H
+	SBB D
+	MOV H, A
+	JC LBL
+	INX B
+	JMP LBL2
+	LBL:
+	DAD D
+	SHLD 8900h
+	MOV L, C
+	MOV H, B
+	SHLD 8902h
 	jmp OVER
 
 OVER:
