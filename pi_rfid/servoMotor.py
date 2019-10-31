d6ac0e4e5ed5f059fd6970722b61375b1b948db5
Kartik Gupta <krtk.anurima123@gmail.com>
2019-10-31 14:04:46 +0000

    Added stepper Motor code

--- pi_rfid/servoMotor.py
+++ pi_rfid/servoMotor.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-control_pins = [7,11,13,15]
+control_pins = [3,5,7,8]
 
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
@@ -19,7 +19,7 @@ halfstep_seq = [
   [1,0,0,1]
 ]
 
-for i in range(512):
+while(4):
 	for halfstep in range(8):
 		for pin in range(4):
 			GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
